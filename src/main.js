a9716456b8e0efc3d5f37424cee19b70e9a2593b
SoulMinT05 <minhtam5111993@gmail.com>
2024-10-29 16:44:31 +0700

    Them trang loi 404

--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Import router
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import { faUser, faCoffee } from '@fortawesome/free-solid-svg-icons'; // Import các biểu tượng mà bạn muốn sử dụng
-
-// Thêm các biểu tượng vào thư viện
-// library.add(faUser, faCoffee);
 
 // Tạo ứng dụng Vue
 const app = createApp(App);
 
-// Đăng ký FontAwesomeIcon như là một component toàn cục
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Sử dụng router
